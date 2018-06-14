c50c5d6cdf357a3257d9c6ff5259afd7b05361a8
Aaron Nicoli <aaronnicoli@gmail.com>
2018-06-14 14:14:32 +0200

    Added the build images script for quick ref

---
+++ build_docker_images.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+
+IMAGE_REGISTRY="aaronnicoli/"
+
+
+IMAGES=( "base" "validator" "settings-tp" "poet-validator-registry" "rest-api" "identity-tp" )
+
+CURDIR=`dirname "$0"`
+
+
+for i in "${IMAGES[@]}"
+do
+  docker build --rm -t ${IMAGE_PREFIX}sawtooth-${i}:latest -f ${CURDIR}/${i}/Dockerfile ${CURDIR}/
+done
