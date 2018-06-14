f5dbe088b69b7b69fb104c230b69ec4808c1f71f
Aaron Nicoli <aaronnicoli@gmail.com>
2018-06-14 14:16:28 +0200

    Fixed typo

--- build_docker_images.sh
+++ build_docker_images.sh
@@ -11,5 +11,5 @@ CURDIR=`dirname "$0"`
 
 for i in "${IMAGES[@]}"
 do
-  docker build --rm -t ${IMAGE_PREFIX}sawtooth-${i}:latest -f ${CURDIR}/${i}/Dockerfile ${CURDIR}/
+  docker build --rm -t ${IMAGE_REGISTRY}sawtooth-${i}:latest -f ${CURDIR}/${i}/Dockerfile ${CURDIR}/
 done
